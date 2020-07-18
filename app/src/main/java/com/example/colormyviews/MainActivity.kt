9546b80de9125914f1fc24c9f1b08c8b67416121
lewvi <56486124+lewvi@users.noreply.github.com>
2020-07-18 16:41:09 +0700

    completed

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         when (view.id) {
 
             // Boxes using Color class colors for the background
-           R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
